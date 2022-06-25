42d76915e0d51a11fbcbfa5b15d8fd44d27d6504
Hexy <r.izmajlov2018@yandex.ru>
2022-06-25 18:59:30 +0400

    add saving to db

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = '2140870532:AAFOoRSk-jYoPAO2VzS1E8WXCmETCNr_xGE'
 APP_URL = 'https://tg-database.herokuapp.com/' + BOT_TOKEN
+DB_URI = 'postgres://xvsrcqachsqepm:8fc3012b116bb502cf88e1540a400253f96634107b6f8cf367f0a3c0be36e8a2@ec2-34-242-84-130.eu-west-1.compute.amazonaws.com:5432/ddqn3dukekljtj'
--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-import os
-import logging
+import psycopg2
 import config
 import telegram
-from flask import Flask, request
 from telegram.ext import Updater, CommandHandler, MessageHandler
 
-def start(update, context):
-    update.message.reply_text('Hi!')
+db_connection = psycopg2.connect(config.DB_URI, sslmode='require')
+db_object = db_connection.cursor()
+
+updater = Updater(config.BOT_TOKEN, use_context=True)
+dp = updater.dispatcher
+
+
+def start(update: Updater, context):
+    msg: telegram.Message = update.message
+    user_id = msg.from_user.id
+    username = msg.from_user.username
+
+    db_object.execute(f'select id from users where id={user_id}')
+    result = db_object.fetchone()
+    if not result:
+        db_object.execute(f'insert into users(id, username, messages) values ({user_id}, {username}, 0)')
+
+    msg.reply_text(f'Добавлен пользователь\nid:{user_id}\nusername:{username}')
+
+
 
 def help(update, context):
     update.message.reply_text('Help!')
 
+
 def main():
-    updater = Updater(config.BOT_TOKEN, use_context=True)
-    dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
 
