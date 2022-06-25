a7989e1e2733b7090516740074e3ab5d26113ed9
Hexy <r.izmajlov2018@yandex.ru>
2022-06-25 17:59:55 +0400

    Bot only

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = '2140870532:AAFOoRSk-jYoPAO2VzS1E8WXCmETCNr_xGE'
+APP_URL = 'https://tg-database.herokuapp.com/' + BOT_TOKEN
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print(123)
+import config
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler
+
+def start(update, context):
+    update.message.reply_text('Hi!')
+
+def help(update, context):
+    update.message.reply_text('Help!')
+
+def main():
+    updater = Updater(config.BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
