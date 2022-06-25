95df3d57b5fedf06d0fb66e5db2cee49b351c89e
Hexy <r.izmajlov2018@yandex.ru>
2022-06-25 18:12:08 +0400

    webhook

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+import logging
 import config
 import telegram
+from flask import Flask, request
 from telegram.ext import Updater, CommandHandler, MessageHandler
 
 def start(update, context):
@@ -13,8 +16,13 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    updater.start_polling()
-    updater.idle()
+
+    if config.APP_URL is None:
+        updater.start_polling()  # врубает бота с локалки
+    else:
+        updater.start_webhook(listen='0.0.0.0', port=int(os.environ.get('PORT', 5000)), url_path=config.BOT_TOKEN)
+        updater.bot.set_webhook(config.APP_URL)  # врубает бота с сервера
+    updater.idle()  # ==while True
 
 
 if __name__ == '__main__':
